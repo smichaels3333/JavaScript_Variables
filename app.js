7ad1e8ad9281b84ac54e533c10a9529b8b8635cb
Stephen Michaels <stephen.michaels3333@gmail.com>
2023-03-28 14:20:09 -0400

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Stephen";
+let lastName = "Michaels";
+var myAge = 27;
+letisDone = false;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log("templateFullName:", templateFullName);
+console.log("fullName: ", fullName);
 
 // Exercise 3
+let myStory;
+let city = "Ocala";
+let pasttime = "lifting";
+
+myStory = `My name is ${fullName}. I live in ${city}. I enjoy ${pasttime}.`;
+console.log(myStory);
